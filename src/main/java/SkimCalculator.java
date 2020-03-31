68bd2480a85036612ddcbfb9869ae408f4c3e059
cllorca1 <carlos.llorca@tum.de>
2020-03-31 16:12:41 +0200

    add filter of rail modes

--- src/main/java/SkimCalculator.java
+++ src/main/java/SkimCalculator.java
@@ -1,6 +1,7 @@
 import ch.sbb.matsim.analysis.skims.CalculateSkimMatrices;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 
 import java.io.IOException;
 import java.util.Random;
@@ -23,14 +24,37 @@ public class SkimCalculator {
         // alternative if you don't have facilities:
         skims.calculateSamplingPointsPerZoneFromNetwork(networkFilename, 1, new Random(0));
         //skims.calculateNetworkMatrices(networkFilename, eventsFilename, timesCar, config, null, link -> true);
-
-
-        skims.calculatePTMatrices(networkFilename, transitScheduleFilename, 8*60*60, 9*60*60, config, null, (line, route) -> route.getTransportMode().equalsIgnoreCase("rail"));
-
+        skims.calculatePTMatrices(networkFilename, transitScheduleFilename, 8*60*60, 9*60*60, config, null, (line, route) -> isRailTramOrSubway(route));
 
         //skims.calculateBeelineMatrix();
 
 
+    }
+
+    /*
+    Use this for tram, metro, to find when these modes are not used.
+     */
+    private static boolean isRailTramOrSubway(TransitRoute route) {
+        boolean returnValue = false;
+        if (route.getTransportMode().equalsIgnoreCase("rail") ||
+                route.getTransportMode().equalsIgnoreCase("tram") ||
+                route.getTransportMode().equalsIgnoreCase("subway")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    Use this for all modes, to find when train is not used
+     */
+    private static boolean isRail(TransitRoute route) {
+        boolean returnValue = false;
+        if (route.getTransportMode().equalsIgnoreCase("rail")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
