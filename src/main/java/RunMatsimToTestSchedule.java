d78f8c15c4a50bf1e2e01426b7c09943d92bbbe4
cllorca1 <carlos.llorca@tum.de>
2020-03-31 13:08:51 +0200

    simple class to run matsim from a config here

---
+++ src/main/java/RunMatsimToTestSchedule.java
@@ -0,0 +1,20 @@
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class RunMatsimToTestSchedule {
+
+
+    public static void main(String[] args) {
+
+        String configFileName = args[0];
+        Config config = ConfigUtils.loadConfig(configFileName);
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        Controler controler = new Controler(scenario);
+        controler.run();
+
+    }
+
+}
