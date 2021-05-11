29cffb49a095e0a65d03c216f676da5c1ec09adc
cllorca1 <carlos.llorca@tum.de>
2021-05-11 08:40:10 +0200

    minor improvement to the class to extract from omx to csv

--- src/main/java/scenarios/ExtractModeVariablesFromOMXToCSV.java
+++ src/main/java/scenarios/ExtractModeVariablesFromOMXToCSV.java
@@ -18,9 +18,14 @@ public class ExtractModeVariablesFromOMXToCSV {
     private IndexedDoubleMatrix2D matrixCarDistance;
     private IndexedDoubleMatrix2D matrixCar;
     private IndexedDoubleMatrix2D matrixTrain;
+    private IndexedDoubleMatrix2D matrixTrainDistance;
     private IndexedDoubleMatrix2D matrixBus;
     private IndexedDoubleMatrix2D matrixBusTransfers;
-    private IndexedDoubleMatrix2D matrixBusShare;
+    private IndexedDoubleMatrix2D matrixBusDistanceShare;
+    private IndexedDoubleMatrix2D matrixBusDistance;
+    private IndexedDoubleMatrix2D matrixBusTimeShare;
+    private IndexedDoubleMatrix2D matrixTrainTimeShare;
+    private IndexedDoubleMatrix2D matrixTrainDistanceShare;
 
     public static void main(String[] args) throws FileNotFoundException {
 
@@ -34,10 +39,17 @@ public class ExtractModeVariablesFromOMXToCSV {
     private void readInput() {
         matrixCarDistance = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixCarDistance, "mat1", 1.0);
         matrixCar = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixCarTime, "mat1", 1.0);
+
         matrixTrain = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixTrain, "travel_time_s", 1.0);
+        matrixTrainDistance = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixTrain, "distance_m", 1.0);
+        matrixTrainDistanceShare = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixTrain, "train_distance_share", 1.0);
+        matrixTrainTimeShare = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixTrain, "train_time_share", 1.0);
+
         matrixBus = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "travel_time_s", 1.0);
         matrixBusTransfers = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "transfer_count", 1.0);
-        matrixBusShare = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "train_distance_share", 1.0);
+        matrixBusDistanceShare = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "train_distance_share", 1.0);
+        matrixBusTimeShare = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "train_time_share", 1.0);
+        matrixBusDistance = OmxSkimsReader.readAndConvertToDoubleMatrix(inputMatrixBus, "distance_m", 1.0);
 
         System.out.println("Matrices were read");
     }
@@ -45,44 +57,53 @@ public class ExtractModeVariablesFromOMXToCSV {
     private void runAnalysis(double shareOfOriginZonesForAnalysis, String outputFileName) throws FileNotFoundException {
         Random random = new Random(0);
         PrintWriter pw = new PrintWriter(new File(outputFileName));
-        pw.println("o,d,dist,mode,time,speed,transfers,share");
+        pw.println("o,d,dist,mode,time,speed,transfers,share_distance,share_time");
         for (int origin : matrixCarDistance.getRowLookupArray()) {
 
             if (random.nextDouble() < shareOfOriginZonesForAnalysis) {
                 for (int destination : matrixCarDistance.getColumnLookupArray()) {
-                    double distance = matrixCarDistance.getIndexed(origin, destination);
+                    double distanceCar = matrixCarDistance.getIndexed(origin, destination);
                     double timeCar = matrixCar.getIndexed(origin, destination);
                     double timeTrain = matrixTrain.getIndexed(origin, destination);
+                    double distanceTrain= matrixTrainDistance.getIndexed(origin, destination);
                     double timeBus = matrixBus.getIndexed(origin, destination);
+                    double distanceBus = matrixBusDistance.getIndexed(origin, destination);
 
-                    double speedCar = timeCar > 0 ? distance / timeCar : -1;
-                    double speedTrain = timeTrain > 0 ? distance / timeTrain : -1;
-                    double speedBus = timeBus > 0 ? distance / timeBus : -1;
+                    double speedCar = timeCar > 0 ? distanceCar / timeCar : -1;
+                    double speedTrain = timeTrain > 0 ? distanceTrain / timeTrain : -1;
+                    double speedBus = timeBus > 0 ? distanceBus / timeBus : -1;
 
                     pw.println(origin + "," +
                             destination + "," +
-                            distance + "," +
+                            distanceCar + "," +
                             "car" + "," +
                             timeCar + "," +
                             speedCar + "," +
-                            "0,0");
+                            "0,1,1");
+
+                    double shareTrainDistance = matrixTrainDistanceShare.getIndexed(origin, destination);
+                    double shareTrainTime = matrixTrainTimeShare.getIndexed(origin, destination);
                     pw.println(origin + "," +
                             destination + "," +
-                            distance + "," +
+                            distanceTrain + "," +
                             "train" + "," +
                             timeTrain + "," +
                             speedTrain + "," +
-                            "0,0");
+                            "0" + "," +
+                            shareTrainDistance + "," +
+                            shareTrainTime);
                     double transfersBus = matrixBusTransfers.getIndexed(origin, destination);
-                    double shareBus = matrixBusShare.getIndexed(origin, destination);
+                    double shareBusDistance = matrixBusDistanceShare.getIndexed(origin, destination);
+                    double shareBusTime = matrixBusTimeShare.getIndexed(origin, destination);
                     pw.println(origin + "," +
                             destination + "," +
-                            distance + "," +
+                            distanceBus + "," +
                             "bus" + "," +
                             timeBus + "," +
                             speedBus + "," +
                             transfersBus + "," +
-                            shareBus);
+                            shareBusDistance + "," +
+                            shareBusTime);
 
                 }
             }
