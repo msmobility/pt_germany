35a5f5abc9506478d7f287294b4ea88c68bd1f4c
Wei <weichieh.huang@tum.de>
2021-12-13 17:54:59 +0100

    updated code to fix pt schedule

---
+++ src/main/java/transitSchedule/CheckTransitRouteShape.java
@@ -0,0 +1,155 @@
+package transitSchedule;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.pt.transitSchedule.TransitScheduleReaderV1;
+import org.matsim.pt.transitSchedule.api.*;
+import skimCalculator.MyCalculateSkimMatrices;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+public class CheckTransitRouteShape {
+
+    private static final Logger log = Logger.getLogger(MyCalculateSkimMatrices.class);
+    private static final Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+    private static TransitScheduleReaderV1 transitScheduleReader;
+    private static final String transitScheduleFile = "Z:/indiv/wei/ld_network_schedule/unmapped_schedule_modified_v1.xml";
+    private static final String outputCsvFile = "Z:/indiv/wei/ld_network_schedule/linesInfoWithDist_new_v2.csv";
+    private static int numScannedStops = 0;
+    private static int numScannedLines = 0;
+    private static int numScannedRoutes = 0;
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        readTransitSchedule();
+        analyzeTransitRoute();
+
+    }
+
+    private static void analyzeTransitRoute() throws FileNotFoundException {
+
+        TransitSchedule transitSchedule = scenario.getTransitSchedule();
+
+        PrintWriter pw = new PrintWriter(outputCsvFile);
+        pw.println("line,route,mode,seq,stop_id,stop_x,stop_y," +
+                "distPrevious2Current,distPrevious2Next,speedPrevious2Current,speedPrevious2Next," +
+                "suspicious,inspected,offRatio,offDiff,speedRatio");
+
+        log.info("Starting calculating.");
+
+        Map<Id<TransitLine>, TransitLine> transitLineMap = transitSchedule.getTransitLines();
+
+        for (Map.Entry<Id<TransitLine>, TransitLine> transitLines : transitLineMap.entrySet()) {
+
+            Map<Id<TransitRoute>, TransitRoute> transitRouteMap = transitSchedule.getTransitLines().get(transitLines.getKey()).getRoutes();
+
+            for (Map.Entry<Id<TransitRoute>, TransitRoute> transitRoutes : transitRouteMap.entrySet()) {
+
+                List<TransitRouteStop> transitStopFacilityMap = transitRouteMap.get(transitRoutes.getKey()).getStops();
+
+                String mode = transitRouteMap.get(transitRoutes.getKey()).getTransportMode();
+                if (transitStopFacilityMap.size() < 2) {
+                    log.info("Line:" + transitLines);
+                    log.info("Route: " + transitRoutes);
+                    log.info("has " + transitStopFacilityMap.size() + " stops.");
+                }
+
+                for (int i = 0; i <= transitStopFacilityMap.size() - 1; i++) {
+
+                    TransitStopFacility currentStop = transitStopFacilityMap.get(i).getStopFacility();
+                    TransitStopFacility previousStop;
+                    TransitStopFacility nextStop;
+
+                    Coord coordCurrentStop = currentStop.getCoord();
+                    Coord coordPreviousStop;
+                    Coord coordNextStop;
+
+                    double distPrevious2Current_m = 0.01;
+                    double distPrevious2Next_m = 0.01;
+
+                    double timeCurrentStop_sec = transitStopFacilityMap.get(i).getDepartureOffset();
+                    double timePreviousStop_sec;
+                    double timeNextStop_sec;
+
+                    double speedPrevious2Current = 0.01;
+                    double speedPrevious2Next = 0.01;
+
+                    boolean isSuspiciousStop = false;
+                    boolean isInspectedStop = false;
+
+                    if (i > 0 && i < transitStopFacilityMap.size() - 1) {
+                        previousStop = transitStopFacilityMap.get(i - 1).getStopFacility();
+                        nextStop = transitStopFacilityMap.get(i + 1).getStopFacility();
+                        coordPreviousStop = previousStop.getCoord();
+                        coordNextStop = nextStop.getCoord();
+                        distPrevious2Current_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordCurrentStop);
+                        distPrevious2Next_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordNextStop);
+                        timePreviousStop_sec = transitStopFacilityMap.get(i - 1).getDepartureOffset();
+                        timeNextStop_sec = transitStopFacilityMap.get(i + 1).getArrivalOffset();
+                        speedPrevious2Current = (distPrevious2Current_m / (timeCurrentStop_sec - timePreviousStop_sec)) * 3.6;
+                        speedPrevious2Next = (distPrevious2Next_m / (timeNextStop_sec - timePreviousStop_sec)) * 3.6;
+
+                        //Todo Check this one first ansd see whether the threshold for speed is reasonable or not
+                        isSuspiciousStop = (((distPrevious2Current_m - distPrevious2Next_m) >= (10 * 1000)) &&
+                                (distPrevious2Current_m / distPrevious2Next_m >= 3));
+
+                        //Todo Check this one second
+//                        isSuspiciousStop = (((distPrevious2Current_m - distPrevious2Next_m) >= (10 * 1000)) &&
+//                                (distPrevious2Current_m / distPrevious2Next_m >= 3)) &&
+//                                (speedPrevious2Current / speedPrevious2Next >= 1.5);
+                        isInspectedStop = true;
+                    } else if (i == transitStopFacilityMap.size() - 1) {
+                        previousStop = transitStopFacilityMap.get(i - 1).getStopFacility();
+                        coordPreviousStop = previousStop.getCoord();
+                        distPrevious2Current_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordCurrentStop);
+                        timePreviousStop_sec = transitStopFacilityMap.get(i - 1).getDepartureOffset();
+                        speedPrevious2Current = (distPrevious2Current_m / (timeCurrentStop_sec - timePreviousStop_sec)) * 3.6;
+                    }
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("\"").append(transitLines.getKey().toString()).append("\"").append(",");
+                    sb.append("\"").append(transitRoutes.getKey().toString()).append("\"").append(",");
+                    sb.append(mode).append(",");
+                    sb.append(i + 1).append(",");
+                    sb.append("\"").append(transitStopFacilityMap.get(i).getStopFacility().getId().toString()).append("\"").append(",");
+                    sb.append(transitStopFacilityMap.get(i).getStopFacility().getCoord().getX()).append(",");
+                    sb.append(transitStopFacilityMap.get(i).getStopFacility().getCoord().getY()).append(",");
+                    sb.append(distPrevious2Current_m).append(",");
+                    sb.append(distPrevious2Next_m).append(",");
+                    sb.append(speedPrevious2Current).append(",");
+                    sb.append(speedPrevious2Next).append(",");
+                    sb.append(isSuspiciousStop).append(",");
+                    sb.append(isInspectedStop).append(",");
+                    sb.append(distPrevious2Current_m / distPrevious2Next_m).append(",");
+                    sb.append(distPrevious2Current_m - distPrevious2Next_m).append(",");
+                    sb.append(speedPrevious2Current / speedPrevious2Next);
+                    pw.println(sb);
+
+                }
+                numScannedStops += transitStopFacilityMap.size();
+            }
+            numScannedRoutes += transitRouteMap.size();
+        }
+        numScannedLines += transitLineMap.size();
+        pw.close();
+        log.info("Calculating completed.");
+        log.info(numScannedLines + " lines are scanned.");
+        log.info(numScannedRoutes + " routes are scanned.");
+        log.info(numScannedStops + " stops are scanned.");
+    }
+
+    private static void readTransitSchedule() {
+        log.info("Starting reading transit schedule.");
+        transitScheduleReader = new TransitScheduleReaderV1(scenario);
+        transitScheduleReader.readFile(transitScheduleFile);
+        log.info("Transit schedule read completely: " + transitScheduleFile);
+    }
+}
---
+++ src/main/java/transitSchedule/ModifyTransitRouteShape.java
@@ -0,0 +1,286 @@
+package transitSchedule;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
+import org.matsim.pt.transitSchedule.TransitScheduleFactoryImpl;
+import org.matsim.pt.transitSchedule.TransitScheduleReaderV1;
+import org.matsim.pt.transitSchedule.TransitScheduleWriterV1;
+import org.matsim.pt.transitSchedule.api.*;
+import skimCalculator.MyCalculateSkimMatrices;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ModifyTransitRouteShape {
+
+
+    private static final Logger log = Logger.getLogger(MyCalculateSkimMatrices.class);
+    private static final Config config = ConfigUtils.createConfig();
+    private static final Scenario scenario = ScenarioUtils.createScenario(config);
+
+    private static final String transitScheduleFile = "Z:/indiv/wei/ld_network_schedule/mapped_schedule.xml";
+
+    private static final String repairListPath = "Z:/indiv/wei/ld_network_schedule/repairList.csv";
+    private static final String outputTransitScheduleFile = "Z:/indiv/wei/ld_network_schedule/unmapped_schedule_modified_v1.xml";
+
+    private static int numModifiedStops = 0;
+    private static int numScannedStops = 0;
+    private static int numScannedLines = 0;
+    private static int numScannedRoutes = 0;
+    private static int newStopId = 1;
+
+    private static TransitSchedule transitSchedule;
+    private static TransitSchedule newTransitSchedule;
+
+    private static Map<Id<TransitStopFacility>, Map<Boolean, Integer>> repairList = new HashMap<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readTransitSchedule();
+        scanTransitStopFacilities();
+        writeRepairList();
+        repairTransitSchedule();
+        generateUnmappedTransitSchedule();
+        writeTransitSchedule();
+    }
+
+    private static void generateUnmappedTransitSchedule() {
+
+        TransitScheduleFactory transitScheduleFactory = new TransitScheduleFactoryImpl();
+        newTransitSchedule = transitScheduleFactory.createTransitSchedule();
+
+        Map<Id<TransitStopFacility>, TransitStopFacility> transitStopMap = transitSchedule.getFacilities();
+
+        for (Map.Entry<Id<TransitStopFacility>, TransitStopFacility> entryStop : transitStopMap.entrySet()) {
+            String originalStopId = entryStop.getValue().getId().toString().split("link")[0];
+            String cleanedStopId = originalStopId.substring(0, originalStopId.length() - 1);
+            Id<TransitStopFacility> newStopId = Id.create(cleanedStopId, TransitStopFacility.class);
+            Coord stopCoord = entryStop.getValue().getCoord();
+            TransitStopFacility newStop = transitScheduleFactory.createTransitStopFacility(newStopId, stopCoord, false);
+            newTransitSchedule.addStopFacility(newStop);
+        }
+
+        Map<Id<TransitLine>, TransitLine> transitLineMap = transitSchedule.getTransitLines();
+
+        for (Map.Entry<Id<TransitLine>, TransitLine> entryLine : transitLineMap.entrySet()) {
+
+            String originalLineId = entryLine.getValue().getId().toString();
+            Id<TransitLine> newLineId = Id.create(originalLineId, TransitLine.class);
+            TransitLine newLine = transitScheduleFactory.createTransitLine(newLineId);
+
+            Map<Id<TransitRoute>, TransitRoute> transitRouteMap = entryLine.getValue().getRoutes();
+
+            for (Map.Entry<Id<TransitRoute>, TransitRoute> entryRoute : transitRouteMap.entrySet()) {
+                String originalRouteId = entryRoute.getValue().getId().toString();
+                Id<TransitRoute> newRouteId = Id.create(originalRouteId, TransitRoute.class);
+
+                List<TransitRouteStop> transitRouteStopList = entryRoute.getValue().getStops();
+                List<TransitRouteStop> newTransitRouteStopList = new ArrayList<>();
+                for (int i = 0; i < transitRouteStopList.size(); i++) {
+
+                    String originalRouteStopId = transitRouteStopList.get(i).getStopFacility().getId().toString().split("link")[0];
+                    String cleanedRouteStopId = originalRouteStopId.substring(0, originalRouteStopId.length() - 1);
+                    Id<TransitStopFacility> newRouteStopId = Id.create(cleanedRouteStopId, TransitStopFacility.class);
+                    double arrivalTime = transitRouteStopList.get(i).getArrivalOffset();
+                    double departureTime = transitRouteStopList.get(i).getDepartureOffset();
+                    TransitStopFacility stopFacility = newTransitSchedule.getFacilities().get(newRouteStopId);
+                    TransitRouteStop newRouteStop = transitScheduleFactory.createTransitRouteStop(stopFacility, arrivalTime, departureTime);
+                    newTransitRouteStopList.add(i, newRouteStop);
+                }
+
+                TransitRoute newRoute = transitScheduleFactory.createTransitRoute(newRouteId, null, newTransitRouteStopList, newRouteId.toString());
+                newRoute.setTransportMode(entryRoute.getValue().getTransportMode());
+
+                Map<Id<Departure>, Departure> departuresMap = entryRoute.getValue().getDepartures();
+                for (Map.Entry<Id<Departure>, Departure> entryDeparture : departuresMap.entrySet()) {
+                    newRoute.addDeparture(entryDeparture.getValue());
+                }
+                newLine.addRoute(newRoute);
+            }
+            newTransitSchedule.addTransitLine(newLine);
+        }
+    }
+
+    private static void repairTransitSchedule() {
+
+        log.info("Start repairing transit stop facilities.");
+
+        Map<Id<TransitLine>, TransitLine> transitLineMap = transitSchedule.getTransitLines();
+
+        for (Map.Entry<Id<TransitLine>, TransitLine> entry : transitLineMap.entrySet()) {
+
+            Map<Id<TransitRoute>, TransitRoute> transitRouteMap = entry.getValue().getRoutes();
+
+            for (Map.Entry<Id<TransitRoute>, TransitRoute> entryRoute : transitRouteMap.entrySet()) {
+
+                List<TransitRouteStop> transitStopFacilityMap = entryRoute.getValue().getStops();
+
+                String mode = entryRoute.getValue().getTransportMode();
+
+                if (mode.equals("bus") || mode.equals("tram")) {
+
+                    for (int i = 1; i < transitStopFacilityMap.size() - 1; i++) {
+
+                        TransitStopFacility currentStop = transitStopFacilityMap.get(i).getStopFacility();
+                        TransitStopFacility previousStop = transitStopFacilityMap.get(i - 1).getStopFacility();
+                        TransitStopFacility nextStop = transitStopFacilityMap.get(i + 1).getStopFacility();
+
+                        Coord coordCurrentStop = currentStop.getCoord();
+                        Coord coordPreviousStop = previousStop.getCoord();
+                        Coord coordNextStop = nextStop.getCoord();
+
+                        double distPrevious2Current_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordCurrentStop);
+                        double distPrevious2Next_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordNextStop);
+
+                        double timeCurrentStop_sec = transitStopFacilityMap.get(i).getDepartureOffset();
+                        double timePreviousStop_sec = transitStopFacilityMap.get(i - 1).getDepartureOffset();
+                        double timeNextStop_sec = transitStopFacilityMap.get(i + 1).getArrivalOffset();
+
+                        double speedPrevious2Current = distPrevious2Current_m / (timeCurrentStop_sec - timePreviousStop_sec);
+                        double speedPrevious2Next = distPrevious2Next_m / (timeNextStop_sec - timePreviousStop_sec);
+
+                        boolean suspiciousStops = (((distPrevious2Current_m - distPrevious2Next_m) >= (10 * 1000)) &&
+                                (distPrevious2Current_m / distPrevious2Next_m >= 3));
+
+                        if (suspiciousStops) {
+
+                            double newX = (coordPreviousStop.getX() + coordNextStop.getX()) / 2;
+                            double newY = (coordPreviousStop.getY() + coordNextStop.getY()) / 2;
+                            Coord newCoord = new Coord(newX, newY);
+
+                            if (repairList.get(currentStop.getId()).get(Boolean.TRUE) > 0 &&
+                                    repairList.get(currentStop.getId()).get(Boolean.FALSE) == 0) {
+
+                                //Stop facality is wrong for all routes
+                                currentStop.setCoord(newCoord);
+
+                                log.info("Line: " + entry +
+                                        "\n Route: " + entryRoute +
+                                        "\n Stop id: " + currentStop.getId().toString() + " has been modified.");
+
+                                numModifiedStops = numModifiedStops + 1;
+
+                            } else if (repairList.get(currentStop.getId()).get(Boolean.TRUE) > 0 &&
+                                    repairList.get(currentStop.getId()).get(Boolean.FALSE) > 0) {
+
+                                Id<TransitStopFacility> newTransitStopFacilityId = Id.create("newStop_" + newStopId + ".link:pt_" + "newStop_" + newStopId, TransitStopFacility.class);
+                                TransitScheduleFactory transitScheduleFactory = new TransitScheduleFactoryImpl();
+                                TransitStopFacility newTransitStopFacility = transitScheduleFactory.createTransitStopFacility(newTransitStopFacilityId, newCoord, false);
+                                transitSchedule.addStopFacility(newTransitStopFacility);
+
+                                entryRoute.getValue().getStops().get(i).setStopFacility(newTransitStopFacility);
+
+                                //Todo also added for stops on the same line/route, think on how to do this
+
+                                newStopId += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        log.info("Modification completed: " + numModifiedStops + " stops are modified after this run.");
+        log.info("New transit stop facility added: " + newStopId + " stops are added after this run.");
+    }
+
+
+    private static void scanTransitStopFacilities() {
+
+        log.info("Start scanning transit stop facilities.");
+
+        Map<Id<TransitLine>, TransitLine> transitLineMap = transitSchedule.getTransitLines();
+
+        for (Map.Entry<Id<TransitLine>, TransitLine> entry : transitLineMap.entrySet()) {
+
+            Map<Id<TransitRoute>, TransitRoute> transitRouteMap = entry.getValue().getRoutes();
+
+            for (Map.Entry<Id<TransitRoute>, TransitRoute> entryRoute : transitRouteMap.entrySet()) {
+
+                List<TransitRouteStop> transitStopFacilityMap = entryRoute.getValue().getStops();
+
+                for (int i = 0; i < transitStopFacilityMap.size(); i++) {
+
+                    TransitStopFacility currentStop = transitStopFacilityMap.get(i).getStopFacility();
+                    Coord coordCurrentStop = currentStop.getCoord();
+                    boolean suspiciousStops = false;
+                    if (i != 0 && i != transitStopFacilityMap.size() - 1) {
+                        TransitStopFacility previousStop = transitStopFacilityMap.get(i - 1).getStopFacility();
+                        TransitStopFacility nextStop = transitStopFacilityMap.get(i + 1).getStopFacility();
+
+                        Coord coordPreviousStop = previousStop.getCoord();
+                        Coord coordNextStop = nextStop.getCoord();
+
+                        double distPrevious2Current_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordCurrentStop);
+                        double distPrevious2Next_m = CoordUtils.calcEuclideanDistance(coordPreviousStop, coordNextStop);
+
+                        double timeCurrentStop_sec = transitStopFacilityMap.get(i).getDepartureOffset();
+                        double timePreviousStop_sec = transitStopFacilityMap.get(i - 1).getDepartureOffset();
+                        double timeNextStop_sec = transitStopFacilityMap.get(i + 1).getArrivalOffset();
+
+                        double speedPrevious2Current = distPrevious2Current_m / (timeCurrentStop_sec - timePreviousStop_sec);
+                        double speedPrevious2Next = distPrevious2Next_m / (timeNextStop_sec - timePreviousStop_sec);
+                        suspiciousStops = (((distPrevious2Current_m - distPrevious2Next_m) >= (10 * 1000)) &&
+                                (distPrevious2Current_m / distPrevious2Next_m >= 3));
+
+                    }
+
+                    repairList.putIfAbsent(currentStop.getId(), new HashMap<>());
+                    repairList.get(currentStop.getId()).putIfAbsent(Boolean.TRUE, 0);
+                    repairList.get(currentStop.getId()).putIfAbsent(Boolean.FALSE, 0);
+
+                    if (suspiciousStops) {
+                        int originalCount = repairList.get(currentStop.getId()).get(Boolean.TRUE);
+                        repairList.get(currentStop.getId()).put(Boolean.TRUE, originalCount + 1);
+                    } else {
+                        int originalCount = repairList.get(currentStop.getId()).get(Boolean.FALSE);
+                        repairList.get(currentStop.getId()).put(Boolean.FALSE, originalCount + 1);
+                    }
+                    numScannedStops += 1;
+                }
+                numScannedRoutes += 1;
+            }
+            numScannedLines += 1;
+        }
+        log.info(numScannedLines + " lines are scanned.");
+        log.info(numScannedRoutes + " routes are scanned.");
+        log.info(numScannedStops + " stops are scanned.");
+    }
+
+    private static void readTransitSchedule() {
+        log.info("Start reading transit schedule.");
+        TransitScheduleReaderV1 transitScheduleReader = new TransitScheduleReaderV1(scenario);
+        transitScheduleReader.readFile(transitScheduleFile);
+        log.info("Transit schedule is read completely.");
+        transitSchedule = scenario.getTransitSchedule();
+    }
+
+    private static void writeTransitSchedule() {
+        log.info("start writing transit schedule.");
+        TransitScheduleWriterV1 transitScheduleWriter = new TransitScheduleWriterV1(newTransitSchedule);
+        transitScheduleWriter.write(outputTransitScheduleFile);
+        log.info("Transit schedule is written completely.");
+    }
+
+    private static void writeRepairList() throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(repairListPath);
+        pw.println("stopId,misplacedStop,okayStop");
+        for (Map.Entry<Id<TransitStopFacility>, Map<Boolean, Integer>> stop : repairList.entrySet()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(stop.getKey().toString()).append(",");
+            sb.append(stop.getValue().get(Boolean.TRUE)).append(",");
+            sb.append(stop.getValue().get(Boolean.FALSE));
+            pw.println(sb);
+        }
+        pw.close();
+    }
+
+}
